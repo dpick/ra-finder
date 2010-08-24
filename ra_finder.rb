85f9974ee1429a3f898922d087063b778d47938b
David Pick <pickdavid@gmail.com>
2010-08-24 14:35:22 -0500

    switched to using google calendar to show floor events

--- ra_finder.rb
+++ ra_finder.rb
@@ -24,14 +24,22 @@ get '/' do
     @url = "http://maps.google.com/maps/api/staticmap?center=#{places[place]['lat']},#{places[place]['long']}\
             &zoom=18&size=400x400&sensor=false&maptype=satellite".gsub(/ /, "")
 
+    @event_url = event_url
     @events = events
 
     haml :index
   else
     @nick_line = "Nick is in #{place}"
+    @event_url = event_url
     @events = events
+
     haml :unkown_tweet
   end
+end
+
+def event_url
+  "http://www.google.com/calendar/embed?height=600&amp;wkst=1&amp;bgcolor=%23FFFFFF&amp;src=o9kv0en2oa8n13pfr1ehepm0d8%40\
+   group.calendar.google.com&amp;color=%238C500B&amp;ctz=America%2FNew_York".gsub(/ /, "")
 end
 
 def events
