b1a7778de9cc4f2712e7e209add966ddf6dc9b99
David Pick <pickdavid@gmail.com>
2010-08-26 12:16:52 -0500

    removed unused code from ra_finder.rb

--- ra_finder.rb
+++ ra_finder.rb
@@ -5,8 +5,6 @@ require 'haml'
 require 'sass'
 require 'factory'
 
-## LOAD CONFIG FILES
-
 places = YAML::load_file("locations.yml")
 
 factory = Ra_finder.new
@@ -23,7 +21,6 @@ get '/' do
   place = twitter.most_recent_tweet
   @events = factory.google_cal.upcoming_events
   @tz = factory.timezone
-  @event_url = event_url
   @todays_events = factory.google_cal.todays_events
 
   if places.keys.include?(place)
@@ -39,11 +36,4 @@ get '/' do
 
     haml :unkown_tweet
   end
-end
-
-## CUSTOM METHODS
-def event_url
-  "http://feed2js.org//feed2js.php?src=http%3A%2F%2Fwww.google.com
-  %2Fcalendar%2Ffeeds%2Fo9kv0en2oa8n13pfr1ehepm0d8%2540group.
-  calendar.google.com%2Fpublic%2Fbasic&num=5&desc=1&utf=y".gsub(/ /, "")
 end
