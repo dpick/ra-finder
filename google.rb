99ef7f8f4b7f96e23683a3da9c72baf5abda39f2
David Pick <pickdavid@gmail.com>
2010-08-27 01:37:05 -0500

    fixed recuring events

--- google.rb
+++ google.rb
@@ -7,7 +7,7 @@ class Google_cal
     config = YAML::load_file("config.yml")
     @service = GCal4Ruby::Service.new
     @service.authenticate(config['gcal_email'], config['gcal_password'])
-    @cal = GCal4Ruby::Calendar.find(@service, {:id => config['gcal_id']})
+    @cal = GCal4Ruby::Calendar.find(@service, config['gcal_id'], {:scope => :first})
   end
 
   @@instance = Google_cal.new
@@ -17,24 +17,29 @@ class Google_cal
   end
 
   def upcoming_events
-    events = @cal.events.select { |event| event.start_time > tomorrow8am }
+    events = GCal4Ruby::Event.find(@cal, "", {:singleevents => true, :range => {:start => tomorrow8am, :end => end_of_year}})
     sort_events(events)
   end
 
   def todays_events
-    events = @cal.events.select {|event| event.start_time > Time.now and event.start_time < tomorrow8am}
+    events = GCal4Ruby::Event.find(@cal, "", {:singleevents => true, :range => {:start => Time.now, :end => tomorrow8am}})
     sort_events(events)
   end
 
   def all_events
-    sort_events(@cal.events)
+    events = GCal4Ruby::Event.find(@cal, "",{:singleevents => true})
+    sort_events(events)
   end
 
   def tomorrow8am
     Time.parse("8:00 am") + 86400
   end
 
+  def end_of_year
+    Time.parse("June 1st 2011")
+  end
+
   def sort_events(events)
-    events.sort! {|x, y| x.start_time <=> y.start_time }
+    events.sort! {|x, y| x.start <=> y.start }
   end
 end
