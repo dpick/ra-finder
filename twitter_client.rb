f61de82ef3dabf1d55dcf32083c8555ae6b2f316
David Pick <pickdavid@gmail.com>
2010-08-29 17:58:16 -0400

    now pulls hashtag for tweet to fix duplicate tweet issue

--- twitter_client.rb
+++ twitter_client.rb
@@ -2,14 +2,6 @@ require 'twitter'
 require 'yaml'
 
 class Twitter_Client
-  def initialize
-    config = YAML::load_file("config.yml")
-    oauth = Twitter::OAuth.new(config['consumer_key'], config['consumer_secret'])
-    oauth.authorize_from_access(config['access_key'], config['access_secret']) 
-
-    @client = Twitter::Base.new(oauth)
-  end
-
   @@instance = Twitter_Client.new
 
   def self.instance
@@ -17,6 +9,7 @@ class Twitter_Client
   end
 
   def most_recent_tweet
-    @client.user_timeline[0][:text]
+    Twitter.timeline('predanfinder')[0].text =~ /#([A-Za-z0-9']*)/
+    return $1
   end
 end
