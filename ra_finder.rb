cefd9213506b6ed7066ad2f3ca236f62ed74f6b3
David Pick <pickdavid@gmail.com>
2010-11-12 16:45:55 -0500

    removed single letter variables

--- ra_finder.rb
+++ ra_finder.rb
@@ -11,32 +11,11 @@ places = YAML::load_file("locations.yml")
 factory = Ra_finder_factory.new
 twitter = factory.twitter
 cal = factory.google_cal
-w = Weather.new
+weather = Weather.new
 
 get '/styles.css' do
   content_type 'text/css', :charset => 'utf-8'
   sass :styles
-end
-
-get '/fun' do
-  place = twitter.most_recent_tweet
-  @events = cal.upcoming_events
-  @todays_events = cal.todays_events
-  @tz = factory.timezone
-  @on_duty = cal.on_duty
-
-  if places.keys.include?(place)
-    
-    @nick_line = "Nick is fucking a goat #{places[place]['prefix']} #{places[place]['label']}"
-
-    @url = factory.url(places[place]['lat'], places[place]['long'])
-
-    haml :index
-  else
-    @nick_line = "Nick is fucking a goat in #{place}"
-
-    haml :unkown_tweet
-  end
 end
 
 get '/' do
@@ -55,8 +34,8 @@ get '/' do
     @on_duty_phone = ""
     @nick_line = "We can't find Nick right now, try his cell phone 317-501-0434"
   end
-  @THTemp = w.getTemp
-  @THFeels = w.getFeelsLike
+  @THTemp = weather.getTemp
+  @THFeels = weather.getFeelsLike
 
   if places.keys.include?(place)
     
